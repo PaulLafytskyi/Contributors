50a1c7c2425b2b092e21da5621d1adfddbdc0e77
Paul Lafytskyi <paul.lafytskyi@gmail.com>
2018-03-03 19:08:46 +0200

    Restructuring project files

---
+++ Contributors/Classes/Application/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Contributors
+//
+//  Created by Poul Lafitskiy on 3/3/18.
+//  Copyright © 2018 PLF. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
